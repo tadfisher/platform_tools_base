1018bd4b4fad443ca68d8a4d49d8edd3256a02a7
Xavier Ducrohet <xav@android.com>
2013-03-05 11:23:03 -0800

    Update sdklib to deal with the new build-tools SDK component.
    Also update the ant task to use this new component.
    
    Change-Id: I9e6a924a6ae15a8eed0a9a44cf1e684d34b71fcd

---
+++ legacy/ant-tasks/src/main/java/com/android/ant/GetBuildToolsTask.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.ant;
+
+import com.android.sdklib.BuildToolInfo;
+import com.android.sdklib.SdkManager;
+import com.android.sdklib.internal.project.ProjectProperties;
+import com.android.sdklib.repository.FullRevision;
+import com.android.utils.NullLogger;
+import com.google.common.collect.Lists;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+public class GetBuildToolsTask extends Task {
+
+    private String mName;
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    @Override
+    public void execute() throws BuildException {
+        Project antProject = getProject();
+
+        SdkManager sdkManager = SdkManager.createManager(
+                antProject.getProperty(ProjectProperties.PROPERTY_SDK), NullLogger.getLogger());
+
+        if (sdkManager == null) {
+            throw new BuildException("Unable to parse the SDK!");
+        }
+
+        BuildToolInfo buildToolInfo = null;
+
+        String buildToolsVersion = antProject.getProperty(ProjectProperties.PROPERTY_BUILD_TOOLS);
+        if (buildToolsVersion != null) {
+            buildToolInfo = sdkManager.getBuildTool(FullRevision.parseRevision(buildToolsVersion));
+        }
+
+        if (buildToolInfo == null) {
+            // get the latest one instead
+            Set<FullRevision> fullRevisionSet = sdkManager.getBuildTools();
+
+            if (fullRevisionSet.isEmpty()) {
+                throw new BuildException("SDK does not have any Build Tools installed.");
+            }
+
+            List<FullRevision> fullRevisionList = Lists.newArrayList(fullRevisionSet);
+            Collections.sort(fullRevisionList);
+
+            FullRevision fullRevision = fullRevisionList.get(fullRevisionList.size() - 1);
+            System.out.println("Using latest Build Tools: " + fullRevision);
+
+
+            buildToolInfo = sdkManager.getBuildTool(fullRevision);
+        }
+
+        antProject.setProperty(mName, buildToolInfo.getLocation().getAbsolutePath());
+    }
+}
