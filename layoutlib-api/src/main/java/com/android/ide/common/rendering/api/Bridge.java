97d709e11195906ac31a19c23595e16d6f05620a
Deepanshu Gupta <deepanshu@google.com>
2013-06-17 17:36:32 -0700

    Add support for RTL locales to layoutlib API
    This changeset updates the layoutlib API to add support for rendering
    right-to-left locales. This bumps the version of the API to 10.
    
    It also loads ICU4J at runtime if present.
    
    Change-Id: Id6805468347008336fee8a4acbbb09987ce52416

--- layoutlib-api/src/main/java/com/android/ide/common/rendering/api/Bridge.java
+++ layoutlib-api/src/main/java/com/android/ide/common/rendering/api/Bridge.java
@@ -32,7 +32,10 @@ import java.util.Map;
  */
 public abstract class Bridge {
 
-    public static final int API_CURRENT = 9;
+    public static final int LAYOUT_LTR = 0;
+    public static final int LAYOUT_RTL = 1;
+
+    public static final int API_CURRENT = 10;
 
     /**
      * Returns the API level of the layout library.
@@ -141,6 +144,15 @@ public abstract class Bridge {
      */
     public Result getViewIndex(Object viewObject) {
         return NOT_IMPLEMENTED.createResult();
+    }
+
+    /**
+     * Returns the character orientation of the locale.
+     * @param locale
+     * @return Returns 0 for LTR and 1 for RTL.
+     */
+    public int getLocaleCharacterOrientation(String locale) {
+        return LAYOUT_LTR;
     }
 
     /**
