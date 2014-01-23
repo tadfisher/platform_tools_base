0e268b08f40b0e92cd4fe50863cd16248b17d505
Xavier Ducrohet <xav@google.com>
2014-01-22 18:01:21 -0800

    Add API to variant.
    Change-Id: I8742b291241ecad1c853fd85f692364f4c0e110f

--- build-system/gradle/src/main/groovy/com/android/build/gradle/api/ApkVariant.java
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/api/ApkVariant.java
@@ -23,9 +23,11 @@ import com.android.build.gradle.tasks.PackageApplication;
 import com.android.build.gradle.tasks.ZipAlign;
 import com.android.builder.DefaultProductFlavor;
 import com.android.builder.model.SigningConfig;
+
 import org.gradle.api.DefaultTask;
 
 import java.io.File;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -74,6 +76,9 @@ public interface ApkVariant extends BaseVariant {
 
     @NonNull
     ZipAlign createZipAlignTask(@NonNull String taskName, @NonNull File inputFile, @NonNull File outputFile);
+
+    Collection<File> getCompileLibraries();
+    Collection<File> getApkLibraries();
 
     /**
      * Returns the installation task.
--- build-system/gradle/src/main/groovy/com/android/build/gradle/internal/api/ApkVariantImpl.java
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/internal/api/ApkVariantImpl.java
@@ -29,6 +29,7 @@ import com.android.builder.model.SigningConfig;
 import org.gradle.api.DefaultTask;
 
 import java.io.File;
+import java.util.Collection;
 import java.util.List;
 
 public abstract class ApkVariantImpl extends BaseVariantImpl implements ApkVariant {
@@ -121,5 +122,16 @@ public abstract class ApkVariantImpl extends BaseVariantImpl implements ApkVaria
         variantData.assembleTask.dependsOn(task);
 
         return task;
+    }
+
+    @Override
+    public Collection<File> getCompileLibraries() {
+        return plugin.getAndroidBuilder(getVariantData()).getCompileClasspath(
+                getVariantData().getVariantConfiguration());
+    }
+
+    @Override
+    public Collection<File> getApkLibraries() {
+        return plugin.getAndroidBuilder(getVariantData()).getPackagedJars(getVariantData().getVariantConfiguration());
     }
 }
