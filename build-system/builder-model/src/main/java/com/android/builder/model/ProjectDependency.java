ea987383e5b30d0387dbdf989286037ffd2b1f12
Xavier Ducrohet <xav@google.com>
2014-02-25 18:12:29 -0800

    Add Project variant name to the AndroidLibrary interface.
    Change-Id: Id75119ee38a689eca9b08407be62f8ff99d9bf67

---
+++ build-system/builder-model/src/main/java/com/android/builder/model/ProjectDependency.java
@@ -0,0 +1,8 @@
+package com.android.builder.model;
+
+/**
+ * Created by xav on 2/25/14.
+ */
+public interface ProjectDependency {
+
+}
---
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/internal/model/ProjectDependencyImpl.java
@@ -0,0 +1,8 @@
+package com.android.build.gradle.internal.model;
+
+/**
+ * Created by xav on 2/25/14.
+ */
+public class ProjectDependencyImpl {
+
+}
