e52f93d0bfd7e354510a4dedac34f582119120a3
Jerome Dochez <jedo@google.com>
2014-02-26 16:43:53 -0800

    initial version of the new manifest merger. WIP.
    Change-Id: I3e73c879d02205ffc1b7919af8610da00cf504cc

---
+++ build-system/manifest-merger/src/main/java/com/android/manifmerger/XmlDocument.java
@@ -0,0 +1,99 @@
+package com.android.manifmerger;
+
+import com.android.utils.PositionXmlParser;
+import com.google.common.base.Optional;
+
+import org.w3c.dom.Element;
+
+import java.io.OutputStream;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+/**
+ * Represents a loaded xml document.
+ *
+ * Has pointers to the root {@link XmlElement} element and provides services to persist the document
+ * to an external format. Also provides abilities to be merged with other
+ * {@link com.android.manifmerger.XmlDocument} as well as access to the line numbers for all
+ * document's xml elements and attributes.
+ *
+ */
+public class XmlDocument {
+
+    private final Element mRootElement;
+    // this is initialized lazily to avoid un-necessary early parsing.
+    private final AtomicReference<XmlElement> mRootNode = new AtomicReference<XmlElement>(null);
+    private final PositionXmlParser mPositionXmlParser;
+    private final XmlLoader.SourceLocation mSourceLocation;
+
+    public XmlDocument(PositionXmlParser positionXmlParser,
+            XmlLoader.SourceLocation sourceLocation,
+            Element element) {
+        this.mPositionXmlParser = positionXmlParser;
+        this.mSourceLocation = sourceLocation;
+        this.mRootElement = element;
+    }
+
+    public boolean write(OutputStream outputStream) {
+
+        try {
+            Transformer tf = TransformerFactory.newInstance().newTransformer();
+            tf.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");         //$NON-NLS-1$
+            tf.setOutputProperty(OutputKeys.ENCODING, "UTF-8");                   //$NON-NLS-1$
+            tf.setOutputProperty(OutputKeys.INDENT, "yes");                       //$NON-NLS-1$
+            tf.setOutputProperty("{http://xml.apache.org/xslt}indent-amount",     //$NON-NLS-1$
+                    "4");                                            //$NON-NLS-1$
+            tf.transform(new DOMSource(getRootNode().getElement()), new StreamResult(outputStream));
+            return true;
+        } catch (TransformerException e) {
+            return false;
+        }
+    }
+
+    // merge this higher priority document with a higher priority document.
+    public Optional<XmlDocument> merge(
+            XmlDocument lowerPriorityDocument,
+            MergingReport.Builder mergingReportBuilder) {
+
+        mergingReportBuilder.getActionRecorder().recordDefaultNodeAction(getRootNode());
+
+        getRootNode().mergeWithLowerPriorityNode(
+                lowerPriorityDocument.getRootNode(), mergingReportBuilder);
+
+        // force re-parsing as new nodes may have appeared.
+        return Optional.of(new XmlDocument(mPositionXmlParser, mSourceLocation, mRootElement));
+    }
+
+    public boolean compareXml(
+            XmlDocument other,
+            MergingReport.Builder mergingReport) throws Exception {
+
+        return getRootNode().compareXml(other.getRootNode(), mergingReport);
+    }
+
+    public PositionXmlParser getPositionXmlParser() {
+        return mPositionXmlParser;
+    }
+
+    public XmlLoader.SourceLocation getSourceLocation() {
+        return mSourceLocation;
+    }
+
+    public synchronized XmlElement getRootNode() {
+        if (mRootNode.get() == null) {
+            this.mRootNode.set(new XmlElement(mRootElement, this));
+        }
+        return mRootNode.get();
+    }
+
+    public String getPackageName() {
+        // TODO(jedo): allow injection...
+        return mRootElement.getAttribute("package");
+    }
+}
---
+++ build-system/manifest-merger/src/main/java/com/android/manifmerger/XmlStringUtils.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.manifmerger;
+
+import com.google.common.base.CaseFormat;
+
+/**
+ * Created by jedo on 2/24/14.
+ */
+public class XmlStringUtils {
+
+    private XmlStringUtils(){}
+
+    public static String constantNameToXmlName(String constantName) {
+        return CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_HYPHEN, constantName);
+    }
+
+    public static String xmlNameToConstantName(String xmlName) {
+        return CaseFormat.LOWER_HYPHEN.to(CaseFormat.UPPER_UNDERSCORE, xmlName);
+    }
+
+    public static String camelCaseToXmlName(String camelCaseName) {
+        return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_HYPHEN, camelCaseName);
+    }
+
+    public static String camelCaseToConstantName(String camelCaseName) {
+        return CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, camelCaseName);
+    }
+}
