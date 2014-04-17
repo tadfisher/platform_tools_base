750e049f0c0d35d7512f014e33cfe91a451ea58d
Raymond Chiu <chiur@google.com>
2014-04-17 13:59:35 -0700

    Create Gradle NDK Integration.
    Change-Id: I7c55c92eb0f4ea2d13de7117fc2cd8e458b9af2a

--- build-system/gradle/src/main/groovy/com/android/build/gradle/BaseExtension.groovy
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/BaseExtension.groovy
@@ -17,6 +17,7 @@ package com.android.build.gradle
 import com.android.SdkConstants
 import com.android.annotations.NonNull
 import com.android.annotations.Nullable
+import com.android.build.gradle.api.AndroidSourceDirectorySet
 import com.android.build.gradle.api.AndroidSourceSet
 import com.android.build.gradle.api.BaseVariant
 import com.android.build.gradle.api.TestVariant
@@ -30,6 +31,7 @@ import com.android.build.gradle.internal.dsl.LintOptionsImpl
 import com.android.build.gradle.internal.dsl.PackagingOptionsImpl
 import com.android.build.gradle.internal.dsl.ProductFlavorDsl
 import com.android.build.gradle.internal.test.TestOptions
+import com.android.build.gradle.ndk.NdkExtension
 import com.android.builder.core.BuilderConstants
 import com.android.builder.core.DefaultBuildType
 import com.android.builder.core.DefaultProductFlavor
@@ -78,6 +80,9 @@ public abstract class BaseExtension {
 
     private String defaultPublishConfig = "release"
     private boolean publishNonDefault = false
+
+    private NdkExtension ndk
+    private boolean useNewNdkPlugin = false
 
     private Closure<Void> variantFilter
 
@@ -429,5 +434,21 @@ public abstract class BaseExtension {
     public void flavorGroups(String... groups) {
         plugin.displayDeprecationWarning("'flavorGroups' has been renamed 'flavorDimensions'. It will be removed in 1.0")
         flavorDimensions(groups);
+    }
+
+    public void ndk(Action<NdkExtension> action) {
+        action.execute(ndk)
+    }
+
+    public boolean getUseNewNdkPlugin() {
+        return useNewNdkPlugin
+    }
+
+    public void setUseNewNdkPlugin(boolean value) {
+        useNewNdkPlugin = value
+    }
+
+    public setNdkExtension(NdkExtension extension) {
+        this.ndk = extension
     }
 }
