93f21e4d915b773204c249834ca911d16ba9a603
Raymond Chiu <chiur@google.com>
2014-04-17 13:59:35 -0700

    Create Gradle NDK Integration.
    Change-Id: I7c55c92eb0f4ea2d13de7117fc2cd8e458b9af2a

---
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/ndk/NdkPlugin.groovy
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.build.gradle.ndk
+
+import com.android.build.gradle.api.AndroidSourceDirectorySet
+import com.android.build.gradle.internal.api.DefaultAndroidSourceDirectorySet
+import com.android.build.gradle.ndk.internal.NdkBuilder
+import com.android.build.gradle.ndk.internal.NdkConfigurationAction
+import com.android.build.gradle.ndk.internal.NdkExtensionConventionAction
+import com.android.build.gradle.ndk.internal.ToolchainConfigurationAction
+import com.android.builder.core.VariantConfiguration
+import com.android.builder.model.BuildType
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.configuration.project.ProjectConfigurationActionContainer
+import org.gradle.internal.Actions
+import org.gradle.internal.reflect.Instantiator
+import org.gradle.nativebinaries.internal.ProjectSharedLibraryBinary
+
+import javax.inject.Inject
+
+/**
+ * Plugin for Android NDK applications.
+ */
+class NdkPlugin implements Plugin<Project> {
+    protected Project project
+    private NdkExtension extension
+    private NdkBuilder ndkBuilder
+    private ProjectConfigurationActionContainer configurationActions
+
+    protected Instantiator instantiator
+
+    @Inject
+    public NdkPlugin(
+            ProjectConfigurationActionContainer configurationActions,
+            Instantiator instantiator) {
+        this.configurationActions = configurationActions
+        this.instantiator = instantiator
+    }
+
+    public Instantiator getInstantiator() {
+        instantiator
+    }
+
+    public NdkExtension getNdkExtension() {
+        extension
+    }
+
+    void apply(Project project) {
+        this.project = project
+
+        def sourceSetContainers = project.container(AndroidSourceDirectorySet) { name ->
+            instantiator.newInstance(DefaultAndroidSourceDirectorySet, name, project.fileResolver)
+        }
+        extension = project.extensions.create("android_ndk", NdkExtension, sourceSetContainers)
+        ndkBuilder = new NdkBuilder(project, extension)
+
+        project.apply plugin: 'c'
+        project.apply plugin: 'cpp'
+
+        configurationActions.add(Actions.composite(
+                new NdkExtensionConventionAction(),
+                new ToolchainConfigurationAction(ndkBuilder, extension),
+                new NdkConfigurationAction(ndkBuilder, extension)))
+    }
+
+    /**
+     * Return the native binary tasks for a VariantConfiguration.
+     */
+    public Iterable<Task> getNdkTasks(VariantConfiguration variantConfig) {
+        if (variantConfig.getType() == VariantConfiguration.Type.TEST) {
+            // Do not return tasks for test variants as test source set is not supported at the
+            // moment.
+            return []
+        }
+        Iterable<ProjectSharedLibraryBinary> binaries =
+                project.binaries.withType(ProjectSharedLibraryBinary).matching  { binary ->
+                        binary.buildType.name.equals(variantConfig.getBuildType().getName()) &&
+                                (variantConfig.getNdkConfig().getAbiFilters() == null ||
+                                        variantConfig.getNdkConfig().getAbiFilters().contains(
+                                                binary.targetPlatform.name))
+        }
+        binaries.collect { ProjectSharedLibraryBinary binary ->
+            project.tasks.getByName(binary.getNamingScheme().getLifecycleTaskName())
+        }
+    }
+
+    /**
+     * Return the output directory of the native binary tasks for a VariantConfiguration.
+     */
+    public File getOutputDirectory(VariantConfiguration variantConfig) {
+        BuildType buildType = variantConfig.buildType
+        new File("$project.buildDir/binaries/${extension.getModuleName()}SharedLibrary/",
+                (variantConfig.type == VariantConfiguration.Type.TEST) ? "test/" : ""
+                        + "$buildType.name/lib")
+    }
+}
---
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/ndk/internal/NdkConfigurationAction.groovy
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.ndk.internal
+
+import com.android.build.gradle.ndk.NdkExtension
+import com.android.builder.core.BuilderConstants
+import org.gradle.api.Action
+import org.gradle.api.Project
+import org.gradle.nativebinaries.BuildType
+import org.gradle.nativebinaries.internal.ProjectSharedLibraryBinary
+import org.gradle.nativebinaries.platform.Platform
+
+/**
+ * Configure settings used by the native binaries.
+ */
+class NdkConfigurationAction implements Action<Project> {
+    NdkExtension ndkExtension
+    NdkBuilder ndkBuilder
+
+    NdkConfigurationAction (NdkBuilder ndkBuilder, NdkExtension ndkExtension) {
+        this.ndkExtension = ndkExtension
+        this.ndkBuilder = ndkBuilder
+    }
+
+    public void execute(Project project) {
+        project.model {
+            buildTypes {
+                maybeCreate(BuilderConstants.DEBUG)
+                maybeCreate(BuilderConstants.RELEASE)
+            }
+        }
+
+        project.libraries {
+            create(ndkExtension.getModuleName())
+        }
+        configureProperties(project)
+
+    }
+
+    void configureProperties(Project project) {
+        project.sources.getByName(ndkExtension.getModuleName()) {
+            c {
+                source {
+                    setSrcDirs(ndkExtension.getSourceSets().getByName(BuilderConstants.MAIN).getSrcDirs())
+                    include "**/*.c"
+                }
+            }
+            cpp {
+                source {
+                    setSrcDirs(ndkExtension.getSourceSets().getByName(BuilderConstants.MAIN).getSrcDirs())
+                    include "**/*.cpp"
+                    include "**/*.cc"
+                }
+            }
+        }
+
+        project.libraries.getByName(ndkExtension.getModuleName()) {
+            binaries.withType(ProjectSharedLibraryBinary.class) { ProjectSharedLibraryBinary binary ->
+                cCompiler.define "ANDROID"
+                cppCompiler.define "ANDROID"
+                cCompiler.define "ANDROID_NDK"
+                cppCompiler.define "ANDROID_NDK"
+
+                // Set output library filename.
+                sharedLibraryFile = new File(
+                        getOutputDirectory(project, buildType, targetPlatform),
+                        "/lib" + ndkExtension.getModuleName() + ".so")
+
+                String sysroot = ndkBuilder.getSysroot(targetPlatform)
+                cCompiler.args  "--sysroot=$sysroot"
+                cppCompiler.args  "--sysroot=$sysroot"
+                linker.args "--sysroot=$sysroot"
+
+                if (ndkExtension.getRenderscriptNdkMode()) {
+                    cCompiler.args "-I$sysroot/usr/include/rs"
+                    cCompiler.args "-I$sysroot/usr/include/rs/cpp"
+                    cppCompiler.args "-I$sysroot/usr/include/rs"
+                    cppCompiler.args "-I$sysroot/usr/include/rs/cpp"
+                    linker.args "-L$sysroot/usr/lib/rs"
+                }
+
+                // Currently do not support customization of stl library.
+                cppCompiler.args "-I${ndkBuilder.getNdkDirectory()}/sources/cxx-stl/stlport/stlport"
+                cppCompiler.args "-I${ndkBuilder.getNdkDirectory()}/sources/cxx-stl//gabi++/include"
+
+                FlagConfiguration flagConfig =
+                        FlagConfigurationFactory.create(ndkBuilder, buildType, targetPlatform
+                        )
+
+                for (String arg : flagConfig.getCFlags()) {
+                    cCompiler.args arg
+                }
+                for (String arg : flagConfig.getCppFlags()) {
+                    cppCompiler.args arg
+                }
+                for (String arg : flagConfig.getLdFlags()) {
+                    linker.args arg
+                }
+
+                // Add flags defined in NdkExtension
+                if (ndkExtension.getcFlags() != null) {
+                    cCompiler.args ndkExtension.getcFlags()
+                }
+                if (ndkExtension.getCppFlags() != null) {
+                    cppCompiler.args ndkExtension.getCppFlags()
+                }
+                for (String ldLibs : ndkExtension.getLdLibs()) {
+                    linker.args "-l$ldLibs"
+                }
+            }
+        }
+    }
+
+    /**
+     * Return the output directory of the native binary.
+     */
+    public File getOutputDirectory(Project project, BuildType buildType, Platform platform) {
+        new File("$project.buildDir/binaries/",
+                "${ndkExtension.getModuleName()}SharedLibrary/$buildType.name/lib/$platform.name")
+    }
+}
