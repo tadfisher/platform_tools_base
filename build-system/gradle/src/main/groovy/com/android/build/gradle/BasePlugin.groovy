d5ffb3d9c5f6e6f0bd18c41356fd61aaff6565f0
Raymond Chiu <chiur@google.com>
2014-04-17 13:59:35 -0700

    Create Gradle NDK Integration.
    Change-Id: I7c55c92eb0f4ea2d13de7117fc2cd8e458b9af2a

--- build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/BasePlugin.groovy
@@ -115,6 +115,7 @@ import com.android.builder.testing.api.TestServer
 import com.android.ide.common.internal.ExecutorSingleton
 import com.android.ide.common.sdk.SdkVersionInfo
 import com.android.utils.ILogger
+import com.android.build.gradle.ndk.NdkPlugin
 import com.google.common.collect.ArrayListMultimap
 import com.google.common.collect.ListMultimap
 import com.google.common.collect.Lists
@@ -1265,7 +1266,9 @@ public abstract class BasePlugin {
         createCompileTask(variantData, testedVariantData)
 
         // Add NDK tasks
-        createNdkTasks(variantData)
+        if (!project.plugins.hasPlugin(NdkPlugin.class)) {
+            createNdkTasks(variantData)
+        }
 
         addPackageTasks(variantData, null, false /*publishApk*/)
 
@@ -1739,7 +1742,15 @@ public abstract class BasePlugin {
                 "package${variantData.variantConfiguration.fullName.capitalize()}",
                 PackageApplication)
         variantData.packageApplicationTask = packageApp
-        packageApp.dependsOn variantData.processResourcesTask, dexTask, variantData.processJavaResourcesTask, variantData.ndkCompileTask
+        packageApp.dependsOn variantData.processResourcesTask, dexTask, variantData.processJavaResourcesTask
+
+        // Add dependencies on NDK tasks if NDK plugin is applied.
+        if (project.plugins.hasPlugin(NdkPlugin.class)) {
+            NdkPlugin ndkPlugin = project.plugins.getPlugin(NdkPlugin.class)
+            packageApp.dependsOn (ndkPlugin.getNdkTasks(variantConfig))
+        } else {
+            packageApp.dependsOn variantData.ndkCompileTask
+        }
 
         packageApp.plugin = this
 
@@ -1754,7 +1765,12 @@ public abstract class BasePlugin {
         packageApp.conventionMapping.jniFolders = {
             // for now only the project's compilation output.
             Set<File> set = Sets.newHashSet()
-            set.addAll(variantData.ndkCompileTask.soFolder)
+            if (project.plugins.hasPlugin(NdkPlugin.class)) {
+                NdkPlugin ndkPlugin = project.plugins.getPlugin(NdkPlugin.class)
+                set.addAll(ndkPlugin.getOutputDirectory(variantConfig))
+            } else {
+                set.addAll(variantData.ndkCompileTask.soFolder)
+            }
             set.addAll(variantData.renderscriptCompileTask.libOutputDir)
             set.addAll(variantConfig.libraryJniFolders)
             set.addAll(variantConfig.jniLibsList)
