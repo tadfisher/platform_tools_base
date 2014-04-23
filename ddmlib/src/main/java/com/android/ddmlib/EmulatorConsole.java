8f18af66dec8e7d0525391ab5489f4c2433db64b
Siva Velusamy <vsiva@google.com>
2014-04-22 17:48:16 -0700

    Fix emulator console returning null every other call
    The EmulatorConsole class maps open consoles to the emulator
    based on the emulator's port number. This leads to issues in the following
    scenario:
     - create mapping from port 5554 -> console 1
     - close emulator at port 5554
     - start new emulator which is now assigned port 5554
     - Attempt to getConsole():
        - this will see that there is already a mapping
        - however, pinging that console will fail
        - so null is returned
    
    This problem occurs because the consoles are not cleaned when an
    emulator gets killed. Since the emulator console itself has no knowledge
    of when an emulator gets killed, this CL adds a new method that can
    be called to close a particular emulator. This API can be called by
    higher level code when it knows that the emulator corresponding to
    a particular console has been killed, or pro-actively close connections
    when not needed.
    
    This possibly fixes https://code.google.com/p/android/issues/detail?id=68069.
    
    Change-Id: I593e3b5e9ec452dfe608c783d1399ad34340deb5

--- ddmlib/src/main/java/com/android/ddmlib/EmulatorConsole.java
+++ ddmlib/src/main/java/com/android/ddmlib/EmulatorConsole.java
@@ -203,6 +203,9 @@ public final class EmulatorConsole {
     /**
      * Returns an {@link EmulatorConsole} object for the given {@link Device}. This can
      * be an already existing console, or a new one if it hadn't been created yet.
+     * Note: emulator consoles don't automatically close when an emulator exists. It is the
+     * responsibility of higher level code to explicitly call {@link #close()} when the emulator
+     * corresponding to a open console is killed.
      * @param d The device that the console links to.
      * @return an <code>EmulatorConsole</code> object or <code>null</code> if the connection failed.
      */
@@ -279,7 +282,6 @@ public final class EmulatorConsole {
     }
 
     private EmulatorConsole(int port) {
-        super();
         mPort = port;
     }
 
@@ -327,12 +329,23 @@ public final class EmulatorConsole {
      */
     public synchronized void kill() {
         if (sendCommand(COMMAND_KILL)) {
-            removeConsole(mPort);
-            try {
+            close();
+        }
+    }
+
+    /**
+     * Closes this instance of the emulator console.
+     */
+    public synchronized void close() {
+        removeConsole(mPort);
+        try {
+            if (mSocketChannel != null) {
                 mSocketChannel.close();
-            } catch (IOException e) {
-                Log.w(LOG_TAG, "Failed to close EmulatorConsole channel");
             }
+            mSocketChannel = null;
+            mPort = -1;
+        } catch (IOException e) {
+            Log.w(LOG_TAG, "Failed to close EmulatorConsole channel");
         }
     }
 
