a8876e294ba5ee84c56673d2dfefddbc7264492c
Jerome Dochez <jedo@google.com>
2014-06-04 13:38:48 -0700

    Wired ability to provide placeholders in build.gradle for manifest merger.
    The new manifest merger is capable of doing placeholder replacement while
    merging the android manifest xml.
    The placeholder key value must be expressed within a flavor declaration like :
        productFlavors {
            free {
            }
            pro {
                manifestPlaceholders = [ activityName:"proName" ]
            }
    
    Change-Id: I880a831412515636da239ddcb4a422d9d0506055

--- build-system/gradle/src/main/groovy/com/android/build/gradle/tasks/MergeManifests.groovy
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/tasks/MergeManifests.groovy
@@ -18,6 +18,7 @@ package com.android.build.gradle.tasks
 import com.android.build.gradle.internal.dependency.ManifestDependencyImpl
 import com.android.builder.core.VariantConfiguration
 import com.android.manifmerger.ManifestMerger2
+import com.google.common.base.Joiner
 import com.google.common.collect.Lists
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
@@ -61,6 +62,17 @@ public class MergeManifests extends ManifestProcessorTask {
     @Input @Optional
     String targetSdkVersion
 
+    /**
+     * Return a serializable version of our map of key value pairs for placeholder substitution.
+     * This serialized form is only used by gradle to compare past and present tasks to determine
+     * whether a task need to be re-run or not.
+     */
+    @Input @Optional
+    String getManifestPlaceholders() {
+        Joiner.on(",").join(variantConfiguration.getMergedFlavor()
+                .getManifestPlaceholders().values());
+    }
+
     VariantConfiguration variantConfiguration
     List<ManifestDependencyImpl> libraries
 
@@ -96,7 +108,7 @@ public class MergeManifests extends ManifestProcessorTask {
                 getMinSdkVersion(),
                 getTargetSdkVersion(),
                 getManifestOutputFile().absolutePath,
-                ManifestMerger2.MergeType.APPLICATION)
+                ManifestMerger2.MergeType.APPLICATION,
+                variantConfiguration.getMergedFlavor().getManifestPlaceholders())
     }
-
 }
