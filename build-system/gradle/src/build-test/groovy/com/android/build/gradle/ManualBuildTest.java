7e503e5c82f9cb5432b02cd294a0bbdb31055706
Jerome Dochez <jedo@google.com>
2014-08-06 10:33:59 -0700

    Use a closure for the classpath for extractAnnotations task
        The extractAnnotations task used to set the classpath in the task creation code
        which happens before the Android SDK has been set. Therefore the renderscript
        support was not available and the classpath was incomplete.
    
        Instead, use a closure to set the classpath which will be re-evaluated each
        time the classpath is looked up. By the time the task actually executes,
        the SDK has been set and the classpath is set correctly.
    
        BUG=72652
    
    Change-Id: I93d3d68048b935cdb9360c7403322e379e93ea0a

--- build-system/gradle/src/build-test/groovy/com/android/build/gradle/ManualBuildTest.java
+++ build-system/gradle/src/build-test/groovy/com/android/build/gradle/ManualBuildTest.java
@@ -50,6 +50,7 @@ import java.util.jar.JarInputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 
 import javax.imageio.ImageIO;
@@ -275,6 +276,29 @@ public class ManualBuildTest extends BuildTest {
 
         // check the resulting .aar file to ensure annotations.zip inclusion.
         File archiveFile = new File(project, "build/outputs/aar/extractAnnotations.aar");
+        assertTrue(archiveFile.isFile());
+        ZipFile archive = null;
+        try {
+            archive = new ZipFile(archiveFile);
+            ZipEntry entry = archive.getEntry("annotations.zip");
+            assertNotNull(entry);
+        } finally {
+            if (archive != null) {
+                archive.close();
+            }
+        }
+    }
+
+    public void testRsEnabledAnnotations() throws IOException {
+        File project = new File(testDir, "extractRsEnabledAnnotations");
+
+        runGradleTasks(sdkDir, ndkDir, BasePlugin.GRADLE_TEST_VERSION,
+                project,
+                Collections.<String>emptyList(),
+                "clean", "assembleDebug");
+
+        // check the resulting .aar file to ensure annotations.zip inclusion.
+        File archiveFile = new File(project, "build/outputs/aar/extractRsEnabledAnnotations.aar");
         assertTrue(archiveFile.isFile());
         ZipFile archive = null;
         try {
