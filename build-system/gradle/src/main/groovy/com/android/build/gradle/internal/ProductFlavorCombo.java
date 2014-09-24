6452bb2d2119a4540f45a30f2426b87295ca58a7
Raymond Chiu <chiur@google.com>
2014-09-24 16:09:12 -0700

    Reorganize logic for android component plugin.
    Changes include:
    - Factor out AndroidComponentModelPlugin.
    - Attach NDK binaries to AndroidBinaries
    - Integrate NdkComponentPlugin and remove NdkPlugin
    - Create tests and properly implement language source set for native
    - Forward compileSdkVersion from AppModelPlugin to Ndk plugin
    - Create AndroidTestBinary for androidTests.
    - Create LibraryComponentModelPlugin
    
    Change-Id: I8b79a28f64e52c588ad30670954c220f28b68ecb

--- build-system/gradle/src/main/groovy/com/android/build/gradle/internal/ProductFlavorCombo.java
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/internal/ProductFlavorCombo.java
@@ -34,7 +34,7 @@ public class ProductFlavorCombo {
     private List<GroupableProductFlavor> flavorList;
 
     /**
-     * Create a ProductFlavorGroup.
+     * Create a ProductFlavorCombo.
      * @param flavors Lists of ProductFlavor.
      */
     public ProductFlavorCombo(GroupableProductFlavor... flavors) {
@@ -70,7 +70,7 @@ public class ProductFlavorCombo {
      * Creates a list containing all combinations of ProductFlavors of the given dimensions.
      * @param flavorDimensions The dimensions each product flavor can belong to.
      * @param productFlavors An iterable of all ProductFlavors in the project..
-     * @return A list of ProductFlavorGroup representing all combinations of ProductFlavors.
+     * @return A list of ProductFlavorCombo representing all combinations of ProductFlavors.
      */
     public static List<ProductFlavorCombo> createGroupList(
             @Nullable List<String> flavorDimensions,
