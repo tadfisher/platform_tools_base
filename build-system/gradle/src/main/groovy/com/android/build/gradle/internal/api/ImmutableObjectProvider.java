4eb7929307c1841fbee818a07cea4acf67f7de49
Xavier Ducrohet <xav@google.com>
2014-10-03 17:16:56 -0700

    Clean up ProductFlavor/BuildType/SigningConfig API.
    1. Enforce Immutability on some items returned in by the variant APIs
    (variant collection and variantFilter)
    
    ProductFlavors, BuildTypes, and SigningConfigs returned by a variant
    through the variant API should be considered read-only. If there are
    not there are some potential issues:
    - the change would be global to the build type, impacting all variants
      using the build type. This is potentially confusing to developers
      who would (most likely based on seeing new users reacting to gradle)
      think it only affects the current variant.
    - the changes may actually not necessarily impact the tasks depending
      on how the tasks are setup, or if task creation is based on the
      property being changed (since this is too late to impact task
      creation).
    
    2. Move NdkConfig out of the base Flavor/BuildType classes.
    This is not ready and shouldn't be included in the builder library.
    
    3. Make signing config part of the Flavor/BuildType based interfaces.
    
    4. Fix ProductFlavor API by having it never returning primitive types.
    This makes it impossible to distinguish between no value set and default
    values which is critical in ensuring that flavors properly merge on top
    of each other (include the default Config).
    
    5. Part of these fixes, the VariantConfiguration class is now using
    generics to help dealing with custom implementations of ProductFlavor
    and BuildType in the Gradle plugin.
    Part of this makes the VariantConfiguration in the builder library
    use more the base interfaces than implementation classes, while
    the Gradle plugin can use more easily the actual implementation
    classes that it uses.
    
    Bug: https://code.google.com/p/android/issues/detail?id=76313
    Change-Id: If7e069e6398f59a6f274fd113a4599a8a04f42b8

---
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/internal/api/ImmutableObjectProvider.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.internal.api;
+
+import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
+import com.android.build.gradle.api.GroupableProductFlavor;
+import com.android.builder.model.BuildType;
+import com.android.builder.model.ProductFlavor;
+import com.android.builder.model.SigningConfig;
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * Provides Immutable versions of BuildType, (Groupable)ProductFlavor and SigningConfig instances
+ * so that they can safely be exposed through the variant API.
+ *
+ * The class creates them on the fly so that they are only created when a
+ * Gradle script/plugin queries for them, and caches them so that we reuse them as needed.
+ */
+public class ImmutableObjectProvider {
+
+    private ImmutableProductFlavor immutableDefaultConfig;
+
+    /**
+     * Map of immutable build-types. This maps the normal build type to the immutable version.
+     */
+    @NonNull
+    private final Map<BuildType, BuildType> immutableBuildTypes = Maps.newIdentityHashMap();
+
+    /**
+     * Map of immutable ProductFlavor. This maps the normal flavor to the immutable version.
+     */
+    @NonNull
+    private final Map<GroupableProductFlavor, GroupableProductFlavor> immutableFlavors = Maps.newIdentityHashMap();
+
+    /**
+     * Map of immutable SigningConfig. This maps the normal config to the immutable version.
+     */
+    @NonNull
+    private final Map<SigningConfig, SigningConfig> immutableSigningConfig = Maps.newIdentityHashMap();
+
+    /**
+     * Returns an immutable version of the default config.
+     * @param defaultConfig the default config.
+     * @return an immutable version.
+     */
+    @NonNull ProductFlavor getDefaultConfig(@NonNull ProductFlavor defaultConfig) {
+        if (immutableDefaultConfig != null) {
+            if (immutableDefaultConfig.productFlavor != defaultConfig) {
+                throw new IllegalStateException("Different DefaultConfigs passed to ApiObjectProvider");
+            }
+        } else {
+            immutableDefaultConfig = new ImmutableProductFlavor(defaultConfig, this);
+        }
+
+        return immutableDefaultConfig;
+    }
+
+    /**
+     * Returns an immutable version of a build type.
+     * @param buildType the build type.
+     * @return an immutable version.
+     */
+    @NonNull
+    public BuildType getBuildType(@NonNull BuildType buildType) {
+        BuildType immutableBuildType = immutableBuildTypes.get(buildType);
+        if (immutableBuildType == null) {
+            immutableBuildTypes.put(buildType,
+                    immutableBuildType = new ImmutableBuildType(buildType, this));
+        }
+
+        return immutableBuildType;
+    }
+
+    /**
+     * Retuens an immutable version of a groupable product flavor.
+     * @param productFlavor the product flavor.
+     * @return an immutable version.
+     */
+    @NonNull
+    public GroupableProductFlavor getProductFlavor(@NonNull GroupableProductFlavor productFlavor) {
+        GroupableProductFlavor immutableProductFlavor = immutableFlavors.get(productFlavor);
+        if (immutableProductFlavor == null) {
+            immutableFlavors.put(productFlavor,
+                    immutableProductFlavor = new ImmutableGroupableProductFlavor(
+                            productFlavor, this));
+        }
+
+        return immutableProductFlavor;
+    }
+
+    /**
+     * Returns an immutable version of a signing config.
+     * @param signingConfig the signing config.
+     * @return an immutable version.
+     */
+    @Nullable
+    public SigningConfig getSigningConfig(@Nullable SigningConfig signingConfig) {
+        if (signingConfig == null) {
+            return null;
+        }
+
+        SigningConfig immutablesigningConfig = immutableSigningConfig.get(signingConfig);
+        if (immutablesigningConfig == null) {
+            immutableSigningConfig.put(signingConfig,
+                    immutablesigningConfig = new ImmutableSigningConfig(signingConfig));
+        }
+
+        return immutablesigningConfig;
+    }
+}
