b9a1f8f41752a0d4c7964747544c0054c4bbe518
Xavier Ducrohet <xav@google.com>
2014-10-20 09:18:59 -0700

    Updated changelog.
    Change-Id: I4cbd1d11e8cddeeaf702d3ed1c18d6186db5c2d3

---
+++ build-system/tests/regular/multiDexWithLib/app/build/generated/source/buildConfig/debug/com/android/tests/basic/BuildConfig.java
@@ -0,0 +1,21 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.android.tests.basic;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.android.tests.basic";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+  // Fields from build type: debug
+  public static final String FOO = "bar";
+  // Fields from default config.
+  /**
+   * @deprecated Use {@link #APPLICATION_ID}
+   */
+  @Deprecated
+  public static final String PACKAGE_NAME = "com.android.tests.basic";
+}
