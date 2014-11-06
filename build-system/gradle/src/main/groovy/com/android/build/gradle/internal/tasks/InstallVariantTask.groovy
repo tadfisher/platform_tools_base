812b73b770b633395885ae2f8d24a7df239827cb
Jerome Dochez <jedo@google.com>
2014-11-06 09:31:56 -0800

    Upgraded tests infrastructure to install ABI splits.
    added a combined densisty abi test.
    
    Change-Id: I7196d1917de916d3586033a5bd43ef34c29c4c6e

--- build-system/gradle/src/main/groovy/com/android/build/gradle/internal/tasks/InstallVariantTask.groovy
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/internal/tasks/InstallVariantTask.groovy
@@ -85,8 +85,12 @@ public class InstallVariantTask extends BaseTask {
                                 "and an ABI in " + Joiner.on(", ").join(device.getAbis()));
                     } else {
                         if (outputFiles.size() > 1) {
-                            project.logger.lifecycle("Multiple APK selected for installation " +
-                                    Joiner.on(", ").join(outputFiles));
+                            List<File> apkFiles = ((List<ApkOutputFile>) outputFiles)*.getOutputFile()
+                            List<String> apkFileNames = apkFiles*.getName()
+                            project.logger.lifecycle("Installing multiple APK '${Joiner.on(", ").join(apkFileNames)}'" +
+                                    " on '${device.getName()}'")
+                            device.installPackages(apkFiles, getTimeOut(), plugin.logger);
+                            successfulInstallCount++
                         } else {
                             File apkFile = ((ApkOutputFile) outputFiles.get(0)).getOutputFile();
                             project.logger.lifecycle(
