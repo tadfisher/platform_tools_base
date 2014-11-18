6ad0522128995a2d224a27ea31b2aee43cb35970
Michal Bendowski <bendowski@google.com>
2014-11-18 15:44:39 +0000

    Rename DSL classes.
    I also changed some types related to SigningConfigs to be more specific and added
    getters with more specific types. There's still some type untangling to be done,
    but that can wait to 1.1.
    
    Change-Id: I605496e94d8c7773d53cdd0bb605f1d6009bdfe3

--- build-system/gradle/src/main/groovy/com/android/build/gradle/AppExtension.groovy
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/AppExtension.groovy
@@ -17,9 +17,9 @@ package com.android.build.gradle
 
 import com.android.build.gradle.api.ApplicationVariant
 import com.android.build.gradle.api.BaseVariant
-import com.android.build.gradle.internal.dsl.BuildTypeDsl
-import com.android.build.gradle.internal.dsl.GroupableProductFlavorDsl
-import com.android.build.gradle.internal.dsl.SigningConfigDsl
+import com.android.build.gradle.internal.dsl.BuildType
+import com.android.build.gradle.internal.dsl.GroupableProductFlavor
+import com.android.build.gradle.internal.dsl.SigningConfig
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.internal.DefaultDomainObjectSet
 import org.gradle.api.internal.project.ProjectInternal
@@ -34,9 +34,9 @@ public class AppExtension extends BaseExtension {
         new DefaultDomainObjectSet<ApplicationVariant>(ApplicationVariant.class)
 
     AppExtension(AppPlugin plugin, ProjectInternal project, Instantiator instantiator,
-                 NamedDomainObjectContainer<BuildTypeDsl> buildTypes,
-                 NamedDomainObjectContainer<GroupableProductFlavorDsl> productFlavors,
-                 NamedDomainObjectContainer<SigningConfigDsl> signingConfigs,
+                 NamedDomainObjectContainer<BuildType> buildTypes,
+                 NamedDomainObjectContainer<GroupableProductFlavor> productFlavors,
+                 NamedDomainObjectContainer<SigningConfig> signingConfigs,
                  boolean isLibrary) {
         super(plugin, project, instantiator, buildTypes, productFlavors, signingConfigs, isLibrary)
     }
