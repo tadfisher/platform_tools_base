6f5eca0c5560b73f94742f9d5ad5a2d7354b14d2
Michal Bendowski <bendowski@google.com>
2015-01-20 22:11:37 +0000

    WIP: ability to run integration tests with Java 7.
    Change-Id: I0a9ca6d74526f6bfd0e82a80d09f39047f896a13

--- build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/common/fixture/GradleTestProject.java
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/common/fixture/GradleTestProject.java
@@ -23,7 +23,6 @@ import static org.junit.Assert.fail;
 import com.android.SdkConstants;
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
-import com.android.build.gradle.BasePlugin;
 import com.android.build.gradle.integration.common.fixture.app.AbstractAndroidTestApp;
 import com.android.build.gradle.integration.common.fixture.app.AndroidTestApp;
 import com.android.build.gradle.integration.common.fixture.app.TestSourceFile;
@@ -639,7 +638,7 @@ public class GradleTestProject implements TestRule {
         ((DefaultGradleConnector) connector).daemonMaxIdleTime(10, TimeUnit.SECONDS);
 
         return connector
-                .useGradleVersion(BasePlugin.GRADLE_TEST_VERSION)
+                .useGradleVersion("2.2")
                 .forProjectDirectory(testDir)
                 .connect();
     }
