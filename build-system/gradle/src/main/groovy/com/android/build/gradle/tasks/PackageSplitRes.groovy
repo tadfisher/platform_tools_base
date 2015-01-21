58988651a0348cf8e83c0832653b0f1586b34f3c
Jerome Dochez <jedo@google.com>
2015-01-21 11:28:22 -0800

    Parallelization of the tasks.
    This will only be functional starting with gradle 2.3
    and adding the following flags :
    -Dorg.gradle.caching.classloaders=true
    -Dorg.gradle.parallel.intra=true
    
    The compile task is not run in parallel so far due to a custom doFirst action.
    
    Change-Id: I3a7059810e8a04e0b0049ef6f2bcc82d07174a96

--- build-system/gradle/src/main/groovy/com/android/build/gradle/tasks/PackageSplitRes.groovy
+++ build-system/gradle/src/main/groovy/com/android/build/gradle/tasks/PackageSplitRes.groovy
@@ -28,6 +28,7 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.ParallelizableTask
 import org.gradle.api.tasks.TaskAction
 
 import java.util.regex.Matcher
@@ -36,6 +37,7 @@ import java.util.regex.Pattern
 /**
  * Package each split resources into a specific signed apk file.
  */
+@ParallelizableTask
 class PackageSplitRes extends BaseTask {
 
     ImmutableList<ApkOutputFile> mOutputFiles;
