22ae214a7cd416206d02828225dd529af2556028
Michal Bendowski <bendowski@google.com>
2015-02-11 13:31:30 +0000

    Implement ProcessJavaResources task for UnitTests.
    This resolves bug #136013
    
    Bug: https://code.google.com/p/android/issues/detail?id=136013
    Change-Id: Ib83e091bbe47a153a7077171ab0652a25520657f

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/TaskManager.groovy
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/TaskManager.groovy
@@ -1070,7 +1070,7 @@ abstract class TaskManager {
         processResources.from(((AndroidSourceSet) variantConfiguration.defaultSourceSet).resources.
                 getSourceFiles())
 
-        if (!variantConfiguration.type.isForTesting()) {
+        if (variantConfiguration.type != ANDROID_TEST) {
             processResources.from(
                     ((AndroidSourceSet) variantConfiguration.buildTypeSourceSet).resources.
                             getSourceFiles())
@@ -1289,9 +1289,11 @@ abstract class TaskManager {
         createPreBuildTasks(variantData)
         createCompileAnchorTask(variantData)
         createCompileTask(variantData, testedVariantData)
+        createProcessJavaResTask(variantData)
 
         // TODO: Unify task creation in 1.2, remove this "special case".
         variantData.javaCompileTask.dependsOn variantData.prepareDependenciesTask
+        variantData.javaCompileTask.dependsOn variantData.processJavaResourcesTask
 
         createJackAndUnitTestVerificationTask(variantData, testedVariantData)
         variantData.assembleVariantTask = createAssembleTask(variantData)
@@ -1492,10 +1494,13 @@ abstract class TaskManager {
             JavaCompile testCompileTask = variantData.javaCompileTask
             runTestsTask.testClassesDir = testCompileTask.destinationDir
 
+            Copy javaResTask = variantData.processJavaResourcesTask
+
             conventionMapping(runTestsTask).map("classpath") {
                 project.files(
                         testCompileTask.classpath,
                         testCompileTask.outputs.files,
+                        javaResTask.outputs.files,
                         androidBuilder.bootClasspath.findAll {
                             it.name != SdkConstants.FN_FRAMEWORK_LIBRARY
                         },
