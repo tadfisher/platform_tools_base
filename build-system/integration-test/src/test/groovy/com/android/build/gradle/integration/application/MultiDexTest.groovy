cce708edfbfc53cdb5f873bc8e5812ee9b4624fe
Chris Warrington <cmw@google.com>
2015-02-24 12:16:54 -0800

    Rename DeviceTests to ConnectedDeviceTests.
    Change-Id: Id0d4ff76f202574068a51d278171f832d828654c

--- build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/MultiDexTest.groovy
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/application/MultiDexTest.groovy
@@ -16,7 +16,7 @@
 
 package com.android.build.gradle.integration.application
 
-import com.android.build.gradle.integration.common.category.DeviceTests
+import com.android.build.gradle.integration.common.category.ConnectedDeviceTests
 import com.android.build.gradle.integration.common.fixture.GradleTestProject
 import com.google.common.io.Files
 import org.junit.AfterClass
@@ -69,7 +69,7 @@ class MultiDexTest {
     }
 
     @Test
-    @Category(DeviceTests.class)
+    @Category(ConnectedDeviceTests.class)
     void connectedCheck() {
         project.execute("connectedCheck")
     }
