0b05e6bd3764b032d26c209626848dd4b2c89537
Michal Bendowski <bendowski@google.com>
2015-02-24 14:33:26 +0000

    Fixes to unit testing and resources.
    * Include prod resources in the classpath.
    * Make sure R class is available in unit tests.
    
    Bug: http://b.android.com/136013
    Change-Id: Ifeee3cfb883207da3a3e6cdb033e01994deca705

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/TaskManager.groovy
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/TaskManager.groovy
@@ -1547,10 +1547,13 @@ abstract class TaskManager {
             project.files(
                     testCompileTask.classpath,
                     testCompileTask.outputs.files,
-                    variantData.processJavaResourcesTask.outputs.files,
+                    variantData.processJavaResourcesTask.outputs,
+                    testedVariantData.processJavaResourcesTask.outputs,
                     androidBuilder.bootClasspath.findAll {
                         it.name != SdkConstants.FN_FRAMEWORK_LIBRARY
                     },
+                    // Mockable JAR is last, to make sure you can shadow the classes with
+                    // dependencies.
                     createMockableJar.outputFile)
         }
 
@@ -1560,7 +1563,6 @@ abstract class TaskManager {
         for (ConfigurableReport report in [testTaskReports.junitXml, testTaskReports.html]) {
             report.destination = new File(report.destination, testedVariantData.name)
         }
-
         tasks.named(JavaPlugin.TEST_TASK_NAME) { Task test ->
             test.dependsOn runTestsTask
         }
