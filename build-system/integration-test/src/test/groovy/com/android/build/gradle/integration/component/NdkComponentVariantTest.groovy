351bf02d3f53c47d9531ee4034cd417287a0eeca
Raymond Chiu <chiur@google.com>
2015-03-10 16:55:50 -0700

    Convert NdkExtension to @Managed type
    Change-Id: I96c69d2935ee5ed6d14ab63440fcb53bb0f043ba

--- build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/component/NdkComponentVariantTest.groovy
+++ build-system/integration-test/src/test/groovy/com/android/build/gradle/integration/component/NdkComponentVariantTest.groovy
@@ -54,7 +54,7 @@ model {
         buildToolsVersion "$GradleTestProject.DEFAULT_BUILD_TOOL_VERSION"
     }
     android.ndk {
-        moduleName "hello-jni"
+        moduleName = "hello-jni"
     }
     android.buildTypes {
         afterEach {
