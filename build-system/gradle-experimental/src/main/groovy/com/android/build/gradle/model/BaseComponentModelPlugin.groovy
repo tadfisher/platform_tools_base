4703fdfc6797b8324a8b70ade85106477fcbaddf
Raymond Chiu <chiur@google.com>
2015-03-10 16:55:50 -0700

    Convert NdkExtension to @Managed type
    Change-Id: I96c69d2935ee5ed6d14ab63440fcb53bb0f043ba

--- build-system/gradle-experimental/src/main/groovy/com/android/build/gradle/model/BaseComponentModelPlugin.groovy
+++ build-system/gradle-experimental/src/main/groovy/com/android/build/gradle/model/BaseComponentModelPlugin.groovy
@@ -44,7 +44,7 @@ import com.android.build.gradle.managed.BuildTypeAdaptor
 import com.android.build.gradle.managed.ManagedBuildType
 import com.android.build.gradle.managed.ManagedSigningConfig
 import com.android.build.gradle.managed.SigningConfigAdaptor
-import com.android.build.gradle.ndk.NdkExtension
+import com.android.build.gradle.ndk.ManagedNdkConfig
 import com.android.build.gradle.tasks.JillTask
 import com.android.build.gradle.tasks.PreDex
 import com.android.builder.core.AndroidBuilder
@@ -285,7 +285,8 @@ public class BaseComponentModelPlugin implements Plugin<Project> {
 
 
         @Mutate
-        void addDefaultAndroidSourceSet(AndroidComponentModelSourceSet sources) {
+        void addDefaultAndroidSourceSet(
+                @Path("android.sources") AndroidComponentModelSourceSet sources) {
             sources.addDefaultSourceSet("resources", AndroidLanguageSourceSet.class);
             sources.addDefaultSourceSet("java", AndroidLanguageSourceSet.class);
             sources.addDefaultSourceSet("manifest", AndroidLanguageSourceSet.class);
@@ -298,15 +299,11 @@ public class BaseComponentModelPlugin implements Plugin<Project> {
 
         @Mutate
         void forwardCompileSdkVersion(
-                @Path("android.ndk") NdkExtension ndkExtension,
+                @Path("android.ndk") ManagedNdkConfig ndkConfig,
                 @Path("android.config") BaseExtension baseExtension) {
-            if (ndkExtension.compileSdkVersion.isEmpty() && baseExtension.compileSdkVersion != null) {
-                ndkExtension.compileSdkVersion(baseExtension.compileSdkVersion);
+            if (ndkConfig.compileSdkVersion.isEmpty() && baseExtension.compileSdkVersion != null) {
+                ndkConfig.compileSdkVersion = baseExtension.compileSdkVersion
             }
-        }
-
-        @Mutate
-        void closeProjectSourceSet(AndroidComponentModelSourceSet sources) {
         }
 
         @Mutate
