87d1d9406ce682a2baee0eca43664dafec38b12c
Michal Bendowski <bendowski@google.com>
2015-03-17 14:39:25 -0700

    Add getRuntimeJavaResourcesFolder to BaseArtifact.
    This way we can start putting it on the classpath when running unit
    tests from AS.
    
    Change-Id: I2abf0b61797e5f24c08d3640b66b76a87ed357b4

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/BaseExtension.groovy
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/BaseExtension.groovy
@@ -526,10 +526,11 @@ public abstract class BaseExtension {
             @NonNull Iterable<String> ideSetupTaskNames,
             @NonNull Configuration configuration,
             @NonNull File classesFolder,
+            @NonNull File runtimeJavaResourceFolder,
             @Nullable SourceProvider sourceProvider) {
         extraModelInfo.registerJavaArtifact(name, variant, assembleTaskName,
                 javaCompileTaskName, generatedSourceFolders, ideSetupTaskNames,
-                configuration, classesFolder, sourceProvider)
+                configuration, classesFolder, runtimeJavaResourceFolder, sourceProvider)
     }
 
     public void registerMultiFlavorSourceProvider(
