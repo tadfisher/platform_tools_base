068d1aa8bed423a3de66627bfa8d9093e3660458
Raymond Chiu <chiur@google.com>
2015-04-06 11:57:21 -0700

    Improve AndroidProject model returned by experimental plugin.
    -Created ComponentModelBuilder to workaround the gradle's lack of
    public API support for ToolingModelRegistry.
    -Added "-fno-limit-debug-info" flag for clang
    -Added "-Wl,--build-id" linker flag.
    -Added dependencies on native binaries for Compile.*Source tasks.
    -Added Truth support for File type.
    
    Change-Id: I2cac924dae2efa01b5e35d51bbba026f73d6aa44

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/Lint.groovy
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/tasks/Lint.groovy
@@ -37,6 +37,7 @@ import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.api.tasks.ParallelizableTask
 import org.gradle.api.tasks.TaskAction
+import org.gradle.tooling.provider.model.ToolingModelBuilder
 import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
 
 @ParallelizableTask
@@ -226,7 +227,7 @@ public class Lint extends DefaultAndroidTask {
 
     private AndroidProject createAndroidProject(@NonNull Project gradleProject) {
         String modelName = AndroidProject.class.getName()
-        ModelBuilder modelBuilder = mToolingRegistry.getBuilder(modelName) as ModelBuilder
+        ToolingModelBuilder modelBuilder = mToolingRegistry.getBuilder(modelName)
         assert modelBuilder != null
         return (AndroidProject) modelBuilder.buildAll(modelName, gradleProject)
     }
