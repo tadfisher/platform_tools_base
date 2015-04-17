05a1dd19f1a897add579f178f3df04cefe2d3610
Chris Warrington <cmw@google.com>
2015-04-17 14:14:52 -0700

    Unify implementation of SourcePosition etc.
    In error message parsing.
    
    Change-Id: Iee95e26784f2c30667c1cfc3adac10c507eb0740

--- common/src/main/java/com/android/ide/common/blame/Message.java
+++ common/src/main/java/com/android/ide/common/blame/Message.java
@@ -17,10 +17,13 @@
 package com.android.ide.common.blame;
 
 import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
 import com.android.annotations.concurrency.Immutable;
 import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 
+import java.io.File;
 import java.util.List;
 
 @Immutable
@@ -35,28 +38,46 @@ public class Message {
     @NonNull
     private final List<SourceFilePosition> mSourceFilePositions;
 
+    @NonNull
+    private final String mRawMessage;
+
     public Message(@NonNull Kind kind,
             @NonNull String text,
             @NonNull SourceFilePosition sourceFilePosition,
             @NonNull SourceFilePosition... sourceFilePositions) {
         mKind = kind;
         mText = text;
+        mRawMessage = text;
+        mSourceFilePositions = ImmutableList.<SourceFilePosition>builder()
+                .add(sourceFilePosition).add(sourceFilePositions).build();
+    }
+
+    public Message(@NonNull Kind kind,
+            @NonNull String text,
+            @NonNull String rawMessage,
+            @NonNull SourceFilePosition sourceFilePosition,
+            @NonNull SourceFilePosition... sourceFilePositions) {
+        mKind = kind;
+        mText = text;
+        mRawMessage = rawMessage;
         mSourceFilePositions = ImmutableList.<SourceFilePosition>builder()
                 .add(sourceFilePosition).add(sourceFilePositions).build();
     }
 
     /*package*/ Message(@NonNull Kind kind,
             @NonNull String text,
-            @NonNull List<SourceFilePosition> positions) {
+            @NonNull String rawMessage,
+            @NonNull ImmutableList<SourceFilePosition> positions) {
         mKind = kind;
         mText = text;
+        mRawMessage = rawMessage;
+  
         if (positions.isEmpty()) {
             mSourceFilePositions = ImmutableList.of(SourceFilePosition.UNKNOWN);
         } else {
             mSourceFilePositions = positions;
         }
     }
-
 
     @NonNull
     public Kind getKind() {
@@ -68,16 +89,67 @@ public class Message {
         return mText;
     }
 
-    /**
-     * Returns a list of sourceFilePositions. Will always contain at least one item.
-     */
     @NonNull
     public List<SourceFilePosition> getSourceFilePositions() {
         return mSourceFilePositions;
     }
 
+    @NonNull
+    public String getRawMessage() {
+        return mRawMessage;
+    }
+
+    @Nullable
+    public String getSourcePath() {
+        if (mSourceFilePositions.isEmpty()) {
+            return null;
+        }
+        File file = mSourceFilePositions.get(0).getFile().getSourceFile();
+        if (file == null) {
+            return null;
+        }
+        return file.getAbsolutePath();
+    }
+
+    /**
+     * Returns a legacy 1-based line number.
+     */
+    @Deprecated
+    public int getLineNumber() {
+        if (mSourceFilePositions.isEmpty()) {
+            return -1;
+        }
+        return mSourceFilePositions.get(0).getPosition().getStartLine() + 1;
+    }
+
+    /**
+     * Returns a legacy 1-based column number.
+     * @return
+     */
+    @Deprecated
+    public int getColumn() {
+        if (mSourceFilePositions.isEmpty()) {
+            return -1;
+        }
+        return mSourceFilePositions.get(0).getPosition().getStartColumn() + 1;
+    }
+
     public enum Kind {
-        ERROR, WARNING, INFO, STATISTICS, UNKNOWN
+        ERROR, WARNING, INFO, STATISTICS, UNKNOWN, SIMPLE;
+
+        public static Kind findIgnoringCase(String s, Kind defaultKind) {
+            for (Kind kind : values()) {
+                if (kind.toString().equalsIgnoreCase(s)) {
+                    return kind;
+                }
+            }
+            return defaultKind;
+        }
+
+        @Nullable
+        public static Kind findIgnoringCase(String s) {
+            return findIgnoringCase(s, null);
+        }
     }
 
     @Override
@@ -101,7 +173,7 @@ public class Message {
 
     @Override
     public String toString() {
-        return Objects.toStringHelper(this).add("kind", mKind).add("text", mText)
-                .add("sources", mSourceFilePositions).toString();
+        return Objects.toStringHelper(this).add("kind", mKind).add("text", mText).add("sources",
+                mSourceFilePositions).toString();
     }
 }
