2e294e00188870d6366163da35577adbcf362c6a
Jerome Dochez <jedo@google.com>
2015-05-13 13:47:23 -0700

    switched to jack/jill API 0.9.0
    Change-Id: I2cbfb6f53a31f74c5a89e47befee3a765f99f9b5

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/dsl/ProductFlavor.groovy
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/dsl/ProductFlavor.groovy
@@ -27,6 +27,7 @@ import com.android.builder.core.DefaultProductFlavor
 import com.android.builder.model.ApiVersion
 import com.android.builder.model.ClassField
 import com.google.common.base.Strings
+import com.google.common.collect.ImmutableList
 import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.logging.Logger
@@ -417,16 +418,15 @@ class ProductFlavor extends DefaultProductFlavor implements CoreProductFlavor {
         return getDimension();
     }
 
+    void jarJarRuleFile(Object file) {
+        setJarJarRuleFiles(ImmutableList.of(project.file(file)))
+    }
 
-    void jarJarRuleFile(String ...files) {
-        if (files.length == 0) {
-            setJarJarRuleFile(null);
-            return;
+    void jarJarRuleFiles(Object ...files) {
+        ImmutableList.Builder<File> builder = ImmutableList.builder()
+        for (String file : files) {
+            builder.add(project.file(file))
         }
-        String jarJarRuleFile = files[0];
-        if (files.length > 1) {
-            logger.error("More than one jarJarRuleFile provided, ignoring all but " + jarJarRuleFile);
-        }
-        setJarJarRuleFile(project.file(jarJarRuleFile))
+        setJarJarRuleFiles(builder.build())
     }
 }
