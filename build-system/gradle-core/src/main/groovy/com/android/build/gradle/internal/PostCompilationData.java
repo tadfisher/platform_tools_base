d76eb6a07881090517e9666c6fc7ae81afd5fe4c
Chris Warrington <cmw@google.com>
2015-05-29 23:50:48 -0700

    Convert TaskManager from Groovy to Java.
    Also remove some unnecessary convention mapping, and use
    java.util.concurrent.Callable and org.gradle.api.Action to replace
    Groovy Closures.
    
    Change-Id: I15085de6f969e254c79f4fe76e065e75017a85c6

---
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/PostCompilationData.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.internal;
+
+import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+/**
+ * Class to hold data to setup the many optional post-compilation steps.
+ */
+public class PostCompilationData {
+
+    @Nullable
+    private List<?> classGeneratingTask;
+
+    @Nullable
+    private List<?> libraryGeneratingTask;
+
+    @Nullable
+    private Callable<List<File>> inputFiles;
+
+    @Nullable
+    private Callable<File> inputDir;
+
+    @Nullable
+    private Callable<File> javaResourcesInputDir;
+
+    @Nullable
+    private Callable<List<File>> inputLibraries;
+
+    private static <T> Callable<T> wrapValue(final T value) {
+        return new Callable<T>() {
+            @Override
+            public T call() throws Exception {
+                return value;
+            }
+        };
+    }
+
+    @Nullable
+    public List<?> getClassGeneratingTask() {
+        return classGeneratingTask;
+    }
+
+    public void setClassGeneratingTask(@Nullable List<?> classGeneratingTask) {
+        this.classGeneratingTask = classGeneratingTask;
+    }
+
+    @Nullable
+    public List<?> getLibraryGeneratingTask() {
+        return libraryGeneratingTask;
+    }
+
+    public void setLibraryGeneratingTask(@Nullable List<?> libraryGeneratingTask) {
+        this.libraryGeneratingTask = libraryGeneratingTask;
+    }
+
+    @Nullable
+    public Callable<List<File>> getInputFiles() {
+        return inputFiles;
+    }
+
+    public void setInputFiles(@Nullable List<File> inputFiles) {
+        this.inputFiles = wrapValue(inputFiles);
+    }
+
+    public void setInputFilesCallable(@Nullable Callable<List<File>> inputFiles) {
+        this.inputFiles = inputFiles;
+    }
+
+    @Nullable
+    public Callable<File> getInputDir() {
+        return inputDir;
+    }
+
+    public void setInputDir(@NonNull File inputDir) {
+        this.inputDir = wrapValue(inputDir);
+    }
+
+    public void setInputDirCallable(@Nullable Callable<File> inputDir) {
+        this.inputDir = inputDir;
+    }
+
+    @Nullable
+    public Callable<File> getJavaResourcesInputDir() {
+        return javaResourcesInputDir;
+    }
+
+    public void setJavaResourcesInputDir(@NonNull File javaResourcesInputDir) {
+        this.javaResourcesInputDir = wrapValue(javaResourcesInputDir);
+    }
+
+    public void setJavaResourcesInputDirCallable(@Nullable Callable<File> javaResourcesInputDir) {
+        this.javaResourcesInputDir = javaResourcesInputDir;
+    }
+
+    @Nullable
+    public Callable<List<File>> getInputLibraries() {
+        return inputLibraries;
+    }
+
+    public void setInputLibraries(@NonNull List<File> inputLibraries) {
+        this.inputLibraries = wrapValue(inputLibraries);
+    }
+
+    public void setInputLibrariesCallable(@Nullable Callable<List<File>> inputLibraries) {
+        this.inputLibraries = inputLibraries;
+    }
+}
