6bc961a27b90e53282f4ac3d44f19f35224a2105
Raymond Chiu <chiur@google.com>
2015-06-11 11:47:45 -0700

    Avoid creating NDK tasks if NDK is not set.
    Validate NDK directory exist if NDK is configured.
    
    Change-Id: I46e11ef6204ef199bf8f72a2dcaa5585b0e2d784

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/NdkHandler.java
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/NdkHandler.java
@@ -201,6 +201,10 @@ public class NdkHandler {
         return ndkDirectory;
     }
 
+    public boolean isNdkDirConfigured() {
+        return ndkDirectory != null;
+    }
+
     private static String getToolchainPrefix(Toolchain toolchain, Abi abi) {
         if (toolchain == Toolchain.GCC) {
             return abi.getGccToolchainPrefix();
@@ -250,8 +254,7 @@ public class NdkHandler {
                 });
 
         if (toolchainPaths == null) {
-            throw new InvalidUserDataException("Unable to find toolchain: "
-                    + prebuiltFolder);
+            throw new InvalidUserDataException("Unable to find toolchain: " + prebuiltFolder);
         }
         if (toolchainPaths.length == 1) {
             return toolchainPaths[0];
