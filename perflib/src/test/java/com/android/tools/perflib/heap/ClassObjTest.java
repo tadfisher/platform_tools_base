bd162546a7c11b49b754bfa9135f656a5eeb6a0d
Chester Hsieh <dunno@google.com>
2015-06-25 11:04:59 -0700

    Fixing issue where ClassObj can't handle duplicate classes.
    Change-Id: I7c4bb822a3480c0f9cf9404dd9b5e4a2edcc1000

--- perflib/src/test/java/com/android/tools/perflib/heap/ClassObjTest.java
+++ perflib/src/test/java/com/android/tools/perflib/heap/ClassObjTest.java
@@ -43,4 +43,16 @@ public class ClassObjTest extends TestCase {
     assertNotNull(dialer);
     assertEquals(5, dialer.getAllFieldsCount());
   }
+
+  public void testComparison() {
+    ClassObj a = new ClassObj(1, null, "TestClassA", 0);
+    ClassObj b = new ClassObj(1, null, "TestClassB", 0);
+    ClassObj c = new ClassObj(2, null, "TestClassC", 0);
+    ClassObj aAlt = new ClassObj(3, null, "TestClassA", 0);
+
+    assertEquals(0, a.compareTo(a));
+    assertEquals(0, a.compareTo(b)); // This is a weird test case, since IDs are supposed to be unique.
+    assertTrue(c.compareTo(a) > 0);
+    assertTrue(aAlt.compareTo(a) > 0);
+  }
 }
