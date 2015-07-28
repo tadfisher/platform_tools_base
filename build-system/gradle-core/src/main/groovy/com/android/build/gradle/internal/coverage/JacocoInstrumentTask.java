8197263b6ab9cc78040cf3101d3f3a47d6cc37ed
Chris Warrington <cmw@google.com>
2015-07-28 14:25:00 +0100

    Make Jacoco instrumentation incremental.
    This replaces the task that ran the jacoco ant task.
    
    It has the disadvantage of tying the jacoco version to the gradle plugin
    version, as it is now a direct dependency. However, users can upgrade jacoco by
    adding a newer version as a build script dependency.
    
    If we want to completly decouple the versions, a separate android jacoco plugin
    seems like the best solution.
    
    Change-Id: Icbb4ece147a9f25c50e2c4bc26e36b1f5bea84da

--- build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/coverage/JacocoInstrumentTask.java
+++ build-system/gradle-core/src/main/groovy/com/android/build/gradle/internal/coverage/JacocoInstrumentTask.java
@@ -14,85 +14,172 @@
  * limitations under the License.
  */
 
-package com.android.build.gradle.internal.coverage
+package com.android.build.gradle.internal.coverage;
 
-import com.android.build.gradle.internal.scope.ConventionMappingHelper
-import com.android.build.gradle.internal.scope.TaskConfigAction
-import com.android.build.gradle.internal.scope.VariantScope
-import com.android.build.gradle.internal.PostCompilationData
-import org.gradle.api.DefaultTask
-import org.gradle.api.file.FileCollection
-import org.gradle.api.tasks.InputDirectory
-import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.TaskAction
+import com.android.build.gradle.internal.scope.ConventionMappingHelper;
+import com.android.build.gradle.internal.scope.TaskConfigAction;
+import com.android.build.gradle.internal.scope.VariantScope;
+import com.android.build.gradle.internal.PostCompilationData;
+import com.android.build.gradle.internal.tasks.IncrementalTask;
+import com.android.ide.common.res2.FileStatus;
+import com.android.utils.FileUtils;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Maps;
+import com.google.common.io.Files;
 
-import static com.android.builder.model.AndroidProject.FD_INTERMEDIATES
+import org.gradle.api.file.ConfigurableFileCollection;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.tasks.InputFiles;
+import org.gradle.api.tasks.OutputDirectory;
+
+import org.jacoco.core.instr.Instrumenter;
+import org.jacoco.core.runtime.OfflineInstrumentationAccessGenerator;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.concurrent.Callable;
 
 /**
  * Simple Jacoco instrument task that calls the Ant version.
  */
-public class JacocoInstrumentTask extends DefaultTask {
+public class JacocoInstrumentTask extends IncrementalTask {
 
-    @InputDirectory
-    File inputDir
+    private File inputDir;
 
-    @OutputDirectory
-    File outputDir
+    private File outputDir;
 
     /**
      * Classpath containing Jacoco classes for use by the task.
      */
-    @InputFiles
-    FileCollection jacocoClasspath
+    private FileCollection jacocoClasspath;
 
-    @TaskAction
-    void instrument() {
-        File outDir = getOutputDir()
-        outDir.deleteDir()
-        outDir.mkdirs()
+    @Override
+    protected boolean isIncremental() {
+        return true;
+    }
 
-        getAnt().taskdef(name: 'instrumentWithJacoco',
-                         classname: 'org.jacoco.ant.InstrumentTask',
-                         classpath: getJacocoClasspath().asPath)
-        getAnt().instrumentWithJacoco(destdir: outDir) {
-            fileset(dir: getInputDir())
+    @Override
+    protected void doFullTaskAction() throws IOException {
+        File outDir = getOutputDir();
+        FileUtils.emptyFolder(outDir);
+
+        Iterable<File> files = Files.fileTreeTraverser().preOrderTraversal(getInputDir()).filter(
+                new Predicate<File>() {
+                    @Override
+                    public boolean apply(File file) {
+                        return file.isFile();
+                    }
+                });
+        Map<File, FileStatus> changedInputs = Maps.newHashMap();
+        for (File file: files) {
+            changedInputs.put(file, FileStatus.NEW);
         }
+        doIncrementalTaskAction(changedInputs);
+
+    }
+
+    @Override
+    protected void doIncrementalTaskAction(Map<File, FileStatus> changedInputs) throws IOException {
+        Instrumenter instrumenter = new Instrumenter(new OfflineInstrumentationAccessGenerator());
+
+        String inputPath = getInputDir().getAbsolutePath();
+        File outputDir = getOutputDir().getAbsoluteFile();
+
+        for (Map.Entry<File, FileStatus> changedInput: changedInputs.entrySet()) {
+            File outputFile = new File(
+                    outputDir,
+                    changedInput.getKey().getAbsolutePath().substring(inputPath.length() + 1));
+            switch (changedInput.getValue()) {
+                case REMOVED:
+                    outputFile.delete();
+                    break;
+                case NEW:
+                    // fall through
+                case CHANGED:
+                    InputStream inputStream = null;
+                    try {
+                        inputStream = Files.asByteSource(changedInput.getKey())
+                                .openBufferedStream();
+                        outputFile.getParentFile().mkdirs();
+                        Files.write(instrumenter.instrument(
+                                inputStream,
+                                changedInput.getKey().toString()), outputFile);
+                    } finally {
+                        if (inputStream != null) {
+                            inputStream.close();
+                        }
+                    }
+            }
+        }
+    }
+
+    @InputDirectory
+    public File getInputDir() {
+        return inputDir;
+    }
+
+    public void setInputDir(File inputDir) {
+        this.inputDir = inputDir;
+    }
+
+    @OutputDirectory
+    public File getOutputDir() {
+        return outputDir;
+    }
+
+    public void setOutputDir(File outputDir) {
+        this.outputDir = outputDir;
+    }
+
+    @InputFiles
+    public FileCollection getJacocoClasspath() {
+        return jacocoClasspath;
+    }
+
+    public void setJacocoClasspath(FileCollection jacocoClasspath) {
+        this.jacocoClasspath = jacocoClasspath;
     }
 
     public static class ConfigAction implements TaskConfigAction<JacocoInstrumentTask> {
 
-        VariantScope scope;
+        private VariantScope scope;
+        private PostCompilationData pcData;
 
-        PostCompilationData pcData;
-
-        ConfigAction(VariantScope scope, PostCompilationData pcData) {
-            this.scope = scope
-            this.pcData = pcData
+        public ConfigAction(VariantScope scope, PostCompilationData pcData) {
+            this.scope = scope;
+            this.pcData = pcData;
         }
 
         @Override
-        String getName() {
+        public String getName() {
             return scope.getTaskName("instrument");
         }
 
         @Override
-        Class<JacocoInstrumentTask> getType() {
-            return JacocoInstrumentTask.class
+        public Class<JacocoInstrumentTask> getType() {
+            return JacocoInstrumentTask.class;
         }
 
         @Override
-        void execute(JacocoInstrumentTask jacocoTask) {
+        public void execute(JacocoInstrumentTask jacocoTask) {
 
-            ConventionMappingHelper.map(jacocoTask, "jacocoClasspath") {
-                scope.globalScope.project.configurations[JacocoPlugin.ANT_CONFIGURATION_NAME]
-            }
+            ConventionMappingHelper.map(jacocoTask, "jacocoClasspath", new Callable<Object>() {
+                @Override
+                public Object call() throws Exception {
+                    return scope.getGlobalScope().getProject().getConfigurations().getByName(JacocoPlugin.ANT_CONFIGURATION_NAME);
+                }
+            });
+
             // can't directly use the existing inputFiles closure as we need the dir instead :\
-            ConventionMappingHelper.map(jacocoTask, "inputDir", pcData.inputDirCallable)
-            ConventionMappingHelper.map(jacocoTask, "outputDir") {
-                new File("${scope.globalScope.buildDir}/${FD_INTERMEDIATES}/coverage-instrumented-classes/${scope.variantConfiguration.dirName}")
-            }
-            scope.variantData.jacocoInstrumentTask = jacocoTask
+            ConventionMappingHelper.map(jacocoTask, "inputDir", pcData.getInputDirCallable());
+
+            jacocoTask.setVariantName(scope.getVariantConfiguration().getFullName());
+            jacocoTask.setOutputDir(scope.getCoverageInstrumentedClassesFolder());
+
+            scope.getVariantData().jacocoInstrumentTask = jacocoTask;
         }
     }
 }
