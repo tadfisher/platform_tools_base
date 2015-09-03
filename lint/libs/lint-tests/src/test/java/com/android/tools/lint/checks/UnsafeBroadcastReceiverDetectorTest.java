293ea15eddf7051a95ab01bea7fa88a9eeb3b71a
Michael Peck <mpeck@mitre.org>
2015-09-03 10:49:45 -0400

    Add lint checks for insecure broadcast receivers
    We propose two new lint checks to detect insecure broadcast receivers that
    fail to properly check the origin of received intents, potentially making
    the receiver vulnerable to spoofed intents.
    
    The first check is for broadcast receivers that have declared an intent-filter
    for a protected-broadcast action string but fail to actually check the received
    action string, as described by section 4.3 of [1].
    
    The second check is for broadcast receivers that have declared an intent-filter
    for the SMS_DELIVER or SMS_RECEIVED action string but fail to ensure
    that the sender holds the BROADCAST_SMS permission. [2] [3]
    
    Note that neither of these checks address receivers dynamically created at runtime.
    They only address receivers that are declared in the application manifest.
    
    [1] Chin, et al. Analyzing Inter-Application Communication in Android.
    Mobisys '11. https://www.eecs.berkeley.edu/~daw/papers/intents-mobisys11.pdf
    
    [2] https://commonsware.com/blog/2013/10/06/secured-broadcasts-sms-clients.html
    
    [3] http://android-developers.blogspot.com/2013/10/getting-your-sms-apps-ready-for-kitkat.html
    
    Change-Id: I2d1a20b2572b332a76d2fbf6cb5b0ff83cc7fab3

---
+++ lint/libs/lint-tests/src/test/java/com/android/tools/lint/checks/UnsafeBroadcastReceiverDetectorTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2015 The MITRE Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.tools.lint.checks;
+
+import com.android.tools.lint.detector.api.Detector;
+
+@SuppressWarnings("javadoc")
+public class UnsafeBroadcastReceiverDetectorTest extends AbstractCheckTest {
+    @Override
+    protected Detector getDetector() {
+        return new UnsafeBroadcastReceiverDetector();
+    }
+
+    public void testBroken() throws Exception {
+        assertEquals(
+            "src/test/pkg/TestReceiver.java:10: Warning: Ensure that the receiver calls " +
+            "getAction to check that the received intent's action string matches the expected " +
+            "value. [UnsafeBroadcastReceiver]\n" +
+            "    public void onReceive(Context context, Intent intent) {\n" +
+            "                ~~~~~~~~~\n" +
+            "0 errors, 1 warnings\n" +
+            "",
+            lintProject(
+                    "unsafereceiver0.xml=>AndroidManifest.xml",
+                    "res/values/strings.xml",
+                    "bytecode/TestReceiver.java.txt=>src/test/pkg/TestReceiver.java",
+                    "bytecode/TestReceiver.class.data=>bin/classes/test/pkg/TestReceiver.class"));
+    }
+
+    public void testBroken2() throws Exception {
+        assertEquals(
+            "AndroidManifest.xml:12: Warning: BroadcastReceivers that declare an intent-filter " +
+            "for SMS_DELIVER or SMS_RECEIVED must ensure that the caller has the BROADCAST_SMS " +
+            "permission, otherwise it is possible for malicious actors to spoof intents. " +
+            "[UnprotectedSMSBroadcastReceiver]\n" +
+            "        <receiver\n" +
+            "        ^\n" +
+            "0 errors, 1 warnings\n" +
+            "",
+            lintProject(
+                    "unsafereceiver1.xml=>AndroidManifest.xml",
+                    "res/values/strings.xml"));
+    }
+
+    public void testCorrect() throws Exception {
+        assertEquals(
+            "No warnings.",
+            lintProject(
+                    "safereceiver0.xml=>AndroidManifest.xml",
+                    "res/values/strings.xml",
+                    "bytecode/TestReceiver2.java.txt=>src/test/pkg/TestReceiver2.java",
+                    "bytecode/TestReceiver2.class.data=>bin/classes/test/pkg/TestReceiver2.class"
+            ));
+    }
+
+    public void testCorrect2() throws Exception {
+        assertEquals(
+            "No warnings.",
+            lintProject(
+                    "safereceiver1.xml=>AndroidManifest.xml",
+                    "res/values/strings.xml"));
+    }
+}
