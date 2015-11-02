e2b6f0e1f41b187a9f902ec2c52d228818f254d2
Michael Peck <mpeck@mitre.org>
2015-11-02 13:39:37 -0500

    Lint check to identify calls to Runtime.load() and System.load()
    This lint check identifies calls to java.lang.Runtime.load()
    and java.lang.System.load(). Those methods can be used to
    dynamically load native code from directories other than the
    platform library directories (generally /system/lib and
    /vendor/lib) or the application's library directory (Android's
    Package Manager at install time moves code from the apk's lib
    directory to an application library directory in /data/app-lib
    that applications themselves cannot write to). This practice
    should be avoided because it increases the risk that the code
    may have been tampered with (for example, if the application
    downloads code to its internal storage directory using
    insecure network protocols after installation, or if the
    application sets unsafe file permissions on code stored in
    its internal storage directories [1]).
    
    Instead, using loadLibrary() is preferred, as it
    provides increased assurance that the libraries are loaded
    from a safer location (the platform library directories or
    the application's library directory).
    
    [1] https://www.nowsecure.com/blog/2015/08/10/world-writable-code-is-bad-mmmmkay/
    
    Change-Id: I86b6b8ffeb2777bbe5b3bcff26b135a53c41fd22

--- lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/BuiltinIssueRegistry.java
+++ lint/libs/lint-checks/src/main/java/com/android/tools/lint/checks/BuiltinIssueRegistry.java
@@ -30,7 +30,7 @@ import java.util.List;
 /** Registry which provides a list of checks to be performed on an Android project */
 public class BuiltinIssueRegistry extends IssueRegistry {
     private static final List<Issue> sIssues;
-    static final int INITIAL_CAPACITY = 232;
+    static final int INITIAL_CAPACITY = 233;
 
     static {
         List<Issue> issues = new ArrayList<Issue>(INITIAL_CAPACITY);
@@ -78,6 +78,7 @@ public class BuiltinIssueRegistry extends IssueRegistry {
         issues.add(DuplicateIdDetector.WITHIN_LAYOUT);
         issues.add(DuplicateResourceDetector.ISSUE);
         issues.add(DuplicateResourceDetector.TYPE_MISMATCH);
+        issues.add(UnsafeNativeCodeDetector.LOAD);
         issues.add(ExtraTextDetector.ISSUE);
         issues.add(FieldGetterDetector.ISSUE);
         issues.add(FullBackupContentDetector.ISSUE);
